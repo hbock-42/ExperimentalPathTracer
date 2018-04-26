b1da2371cda6bb6b8d25635135b02ad6a06eec8a
HBock <42hugobock@gmail.com>
2018-04-26 22:06:28 +0200

    Trying to add sdl

--- ExperimentalPathTracer/Headers/EPT.hpp
+++ ExperimentalPathTracer/Headers/EPT.hpp
@@ -1,3 +1,9 @@
 #pragma once
 
+// External include
+#include <SDL.h>
+#include <iostream>
+
+// Projects include
 #include "Geometry/Vector3.hpp"
+#include "Debug/Debug.hpp"
---
+++ ExperimentalPathTracer/main.cpp
@@ -0,0 +1,32 @@
+
+#include "Headers/EPT.hpp"
+
+
+//int		Initialisation()
+//{
+//	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0)
+//	{
+//		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+//		return 1;
+//	}
+//	Debug::Log("SDL initialized");
+//	return 0;
+//}
+//
+//void	TestVector3()
+//{
+//	Vector3 v31 = Vector3(1, 1, 1);
+//	Vector3 vup = Vector3(0, 1, 0);
+//	Vector3 vright = Vector3(1, 0, 0);
+//	std::cout << v31 << std::endl;
+//	std::cout << "Magnitude = " << v31.Magnitude() << std::endl;
+//	std::cout << "Dot = " << Vector3::Dot(vup, vright) << std::endl;
+//	std::cout << "Magnitude after X 2 = " << (v31 * 2).Magnitude() << std::endl;
+//	std::cout << "Magnitude after X 2 = " << (2 * v31).Magnitude() << std::endl;
+//}
+
+int		main()
+{
+	//Initialisation();
+	return 0;
+}
